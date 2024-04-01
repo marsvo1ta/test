4aaefbacbd470499af02731217d7053ec14b671a
marsvo1ta <euromobilco@gmail.com>
2024-04-01 17:52:08 +0300

    first

---
+++ remove_bg.py
@@ -0,0 +1,17 @@
+# from rembg import remove
+# from PIL import Image
+#
+# # Store path of the image in the variable input_path
+# input_path = 'randog.jpg'
+#
+# # Store path of the output image in the variable output_path
+# output_path = 'son_bg.png'
+#
+# # Processing the image
+# input = Image.open(input_path)
+#
+# # Removing the background from the given Image
+# output = remove(input)
+#
+# # Saving the image in the given path
+# output.save(output_path)
